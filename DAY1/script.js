1269203d93838978efb5c0c32aec0ca363d52b23
Sakhare Divya <sakhredivya@gmail.com>
2025-07-11 03:56:48 -0700

    first commit

---
+++ DAY1/script.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+console.warn("this is warning message");
+
+console.error("this is warning message");
+
+console.info("Welcome");
+
+// console.clear()
+
---
+++ DAY2/script.js
@@ -0,0 +1,42 @@
+// variables
+// var name ="Divya"
+// let name1 = "Om" 
+// const PI = 3.14
+// console.log(PI);
+
+
+// let appName
+// let app_name
+// alert("hii")
+
+
+
+// Data types
+// 1> primitive ----->  Number, string, boolean, undefine, null
+// 2> non primitive----> Array, 
+
+let age = "Divya"
+const PI= 3.00
+console.log(typeof PI)
+
+Boolean
+
+let isValidUser = true
+let IS_OTP_VERIFY = false
+let IS_USER_LOGIN = false
+
+console.log(typeof isValidUser)
+
+
+undefined
+
+let user 
+console.log(typeof user);
+
+
+null
+
+let abc= null
+console.log(typeof abc);
+
+
