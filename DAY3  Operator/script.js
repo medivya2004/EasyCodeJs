0f733013d06993ca491a49315186400b8fa9a270
Sakhare Divya <sakhredivya@gmail.com>
2025-07-12 04:06:04 -0700

    feat: add initial HTML and JavaScript files for Day 2 and Day 3 exercises

---
+++ DAY3  Operator/script.js
@@ -0,0 +1,117 @@
+// 1> Arithmetic Operator
+// + Operator
+
+// let first_number = 100
+// let sec_number = 200
+
+// let result =  first_number + sec_number
+// console.log(result);
+
+ 
+// // - substraction
+
+// let first = 200
+// let second = 100
+// // let result1 =  first - second
+// // console.log(result1);
+// console.log(first-second);
+
+
+// * multiplication
+
+
+// let first = 200
+// let second = 100
+// console.log(first * second);
+
+
+// / Division
+
+// let first = 200
+// let second = 100
+// console.log(first / second);
+// console.log(10/2);
+
+
+// % modulus
+
+// let first = 200
+// let second = 100
+// console.log(first % second);
+
+// increment
+
+// let abc = 0
+// console.log(++abc);
+// console.log(abc);
+
+//decrement
+
+// let abc = 5
+// console.log(abc--);
+// console.log(abc);
+
+// 2> Assignment operator
+
+// = assign operator
+
+// let abc = 12
+// let xyz = "Divya"
+// console.log(xyz);
+
+// add & assign
+
+// let x = 10
+// x += 1
+// console.log(x);
+
+
+// substact & assign
+// let x = 10
+// x -= 1
+// console.log(x);
+
+
+// multiplication & assign
+// let x = 10
+// x *= 2
+// console.log(x);
+
+
+// division & assign
+// let x = 20
+// x /= 2
+// console.log(x);
+
+// comprission operator ==
+
+let abc = 10
+let xyz = 20
+console.log(abc == xyz);
+
+
+let first_number = 100
+let sec_number = `100`
+
+console.log(first_number == sec_number);
+
+
+let a = 10
+let b = "10"
+console.log(a == b);
+console.log(a === b);
+console.log(typeof b);
+
+
+let x = 10
+let y = 20
+console.log(x != y);
+ 
+
+ 
+
+
+
+
+
+
