8fbd96c92bf4e46502b3323e26947554b751993f
Sakhare Divya <sakhredivya@gmail.com>
2025-07-13 03:12:18 -0700

    feat: add HTML and JavaScript files for Day 4 exercises

---
+++ DAY4 Operator2/script.js
@@ -0,0 +1,43 @@
+// Grater than
+// console.log(9>4);
+
+// let a=10
+// let b= 20
+// console.log(a<b);
+
+// console.log(5<=5);
+  
+
+// Logical Operator
+
+// logical and &&
+// 1 + 1 = 1
+// 0 + 1 = 0
+// 1 + 0 = 0
+// 0 + 0 = 0
+
+// let result = true && false
+// console.log(result);
+
+
+// logical or ||
+// 1 + 1 = 1
+// 0 + 1 = 1
+// 1 + 0 = 1
+// 0 + 0 = 0
+
+// let result = true || false
+// console.log(result);
+
+
+// Ternary Operator
+// syntax -> condition ? if condition is true : if condition is false
+
+// 12 == 12 ? console.log("condition is true") : console.log("condition id false");
+// 10 == 12 ? console.log("condition is true") : console.log("condition id false");
+
+let age = 15
+age >=18 ? console.log("you are able to vote") : console.log("you are not  able to vote");
+
+
+
