29c61923ac76b77f934f9cdfb83c0a43095f9e5e
Sakhare Divya <sakhredivya@gmail.com>
2025-07-14 02:34:52 -0700

    feat: add HTML and JavaScript files for Day 5 exercises

---
+++ DAY5 conditional statement/script.js
@@ -0,0 +1,62 @@
+// alert("hii")
+
+// conditional statement
+// if (1==2)
+//  {
+//    console.log("condition is true");
+// }
+// console.log("after the if statement");
+
+// let USER_ROLE = "admin"
+
+// if(USER_ROLE == "admin")
+// {
+//     console.log('welcome admin');
+// }
+
+// let USER_ROLE ="om"
+// if(USER_ROLE == "user")
+// {
+//     console.log('welcome admin');
+// }
+
+// even odd program
+
+// const number = 453434
+// if(number % 2 == 0)
+// {
+//     console.log(number , 'number is even');
+// }
+
+// if else
+
+// const number = 45343987
+// if(number % 2 == 0)
+// {
+//     console.log('Hello I am from if block');
+// }
+// else
+// {
+//    console.log('Hello I am from else block');
+// }
+
+// let number = prompt("enter the number : ")
+// if (number % 2 == 0)
+// {
+//     console.log(number , 'number is even');
+// }
+// else
+// {
+//  console.log(number , 'number is odd');
+// }
+
+let token = " 1237623ehdbhejd7e526yf"
+if(token)
+{
+    console.log("login");
+}
+else
+{
+  console.error("cant find token");
+  
+}
