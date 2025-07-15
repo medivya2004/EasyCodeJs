791fa92691f37f59813ef56a371e68ea054e5491
Sakhare Divya <sakhredivya@gmail.com>
2025-07-15 04:12:19 -0700

    feat: add HTML and JavaScript files for Day 7 and Day 8 exercises

---
+++ DAY7/script.js
@@ -0,0 +1,40 @@
+// while(1==1)
+// {
+//     console.log("Hello");
+    
+// }
+
+// let count =10
+// while(count < 100)
+// {
+//     console.log(count);
+//     count ++;
+// }
+
+// let number = 0;
+// while(number % 2 == 0)
+// {
+//     console.log(number);
+//     number ++;
+// }
+
+
+
+
+// let number = 0;
+// while(number <=100)
+// {   
+//     if(number % 2 ==0)
+//     console.log(number);
+//  number ++;
+// }
+
+
+// do while
+ 
+let count =0
+do{
+  console.log(count);
+  count++
+}
+while(count<-1)
---
+++ DAY8/script.js
@@ -0,0 +1,55 @@
+// do{
+//  console.log("Hello");
+//  }
+// while (false){
+// }
+
+// do
+// {
+//     console.log("Hii");
+//     console.log("Hello");
+// }
+// while(false)
+
+
+    // let count = 0;
+    // do{
+    //     console.log(count);
+    //     count++
+    // }while(count<=5)
+
+
+    // let number = parseInt(prompt("enter the number"));
+    // let multiplier_value=1
+    // while(multiplier_value <= 10)
+    // {
+    //     console.log(multiplier_value * number);
+    //     multiplier_value ++
+    // }
+
+    // for loop
+
+    // let i=0
+    //  for(i;i<=10;i++)
+    //  {
+    //     console.log(i);
+    // }
+
+
+    // let count =0;
+    // let multiplier_value =1
+    // for(multiplier_value ; multiplier_value <=10 ; multiplier_value++)
+    // {
+    //     console.log(multiplier_value * count)
+    // }
+
+    // create a program for sum of first 25 number
+
+    let number =0;
+    let sum = 0;
+    for(number ; number <= 100 ; number++)
+    {
+        sum = sum + number
+    }
+    console.log(sum);
+    
