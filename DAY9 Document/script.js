9f6e3bd227c99c100d47f51aa1846b9c9496ada6
Sakhare Divya <sakhredivya@gmail.com>
2025-07-16 04:07:20 -0700

    feat: add initial HTML and JavaScript files for Day 9 exercises

---
+++ DAY9 Document/script.js
@@ -0,0 +1,33 @@
+// let title = document.title
+// console.log(title);
+
+// document.title = " DIVYA"
+
+// document.write("we learn javascript with easycode")
+// document.write("<h1>Welcome</h1>")
+
+// let number = parseInt(prompt("enter the number :"))
+
+// document.title = "table of" + number
+
+// for (let i = 1; i <=10; i++)
+// {
+//     let op=i * number
+//     // document.writeln(op)
+//     document.writeln("<h5>" + op + "</h5>")
+// }
+
+
+
+let userName = "EasyCode"
+let websiteURL = "www.easycode.support"
+
+console.log(`hello ${userName} welcome  ${websiteURL}`);
+console.log(`hello my friend  ${8+3}`);
+
+
+for(let i = 0; i>= -20; i--)
+{
+    console.log(`i is : ${i}`);
+    
+}
